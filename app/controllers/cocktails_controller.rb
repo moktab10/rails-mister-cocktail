bd376adba62d6c6657d66d1945489cf428aa444e
Mokhtar BOUSSEFSAF <moktab10@gmail.com>
2020-05-18 12:44:53 +0200

    mr cocktail without reviews

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,13 @@
 class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
+    @search = params["search"]
+    if @search.present?
+      @name = @search["name"]
+      @cocktails = Cocktail.where("name ILIKE ?", "%#{@name}%")
+    else
+      @cocktails = Cocktail.all
+    end
   end
 
   def show
